b15c19631ae2a7d6c8168f081035a562bd964f32
putriwulandari2 <a710230001@student.ums.ac.id>
2024-06-07 17:42:12 +0700

    hari2

---
+++ hari2.py
@@ -0,0 +1,24 @@
+print("pilih bentuk")
+print ("1.persegi panjang")
+print ("2.lingkaran")
+print ("3.segitiga")
+print ("4..keluar")
+
+print ("hitung luas persegi panjang")
+panjang = float(input ("masukkan panjang: "))
+lebar =float(input ("masukkan lebar: "))
+luas = (2*panjang*lebar)
+print ("luas persegi panjang adalah", luas)
+
+print("hitung luas lingkaran")
+r =float(input("masukkan jari-jari: "))
+luas =3.14*(r**2)
+print("luas lingkaran adalah", luas)
+
+print("hitung luas segitiga")
+a =float(input ("masukkan alas: "))
+t=float(input("masukkan tinggi: "))
+luas =(a*t)/2
+print("luas segitiga adalah", luas)
+
+
