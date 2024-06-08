53a8f0f8d2e3b9a9bdad3eff0255328addda4d2a
putriwulandari2 <a710230001@student.ums.ac.id>
2024-06-08 17:40:09 +0700

    hari3

---
+++ hari3.py
@@ -0,0 +1,6 @@
+num1 = input("Masukkan angka pertama: ")
+num2 = input("Masukkan angka kedua: ")
+
+sum = float(num1) + float(num2)
+
+print("Jumlah dari {0} dan {1} adalah {2}".format(num1, num2, sum))
