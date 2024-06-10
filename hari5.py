aa181a54e43bc2e9b442362c663fb56bcb05a625
putriwulandari2 <a710230001@student.ums.ac.id>
2024-06-10 18:05:31 +0700

    hari5

---
+++ hari5.py
@@ -0,0 +1,11 @@
+my_string = "Hello, Everyone!"
+my_list = [1, 2, 3, 4, 5]
+my_tuple = (6, 7, 8, 9, 10)
+my_set = {11, 12, 13, 14, 15}
+my_dict = {"name": "Dania", "age": 20, "city": "Yogyakarta"}
+
+print(len(my_string)) 
+print(len(my_list)) 
+print(len(my_tuple)) 
+print(len(my_set)) 
+print(len(my_dict))
